c89aa211879889143835d6ca5ee782a69ddf6b20
mannudfm <mannudhfm@gmail.com>
2024-01-27 16:55:30 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import About from './Components/About';
-import Login from './Components/Login';
+// import Login from './Components/Login';
 import NavBar from './Components/NavBar';
 import Product from './Components/Product';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -10,8 +10,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' exact element={<Login />}></Route>
-        <Route path='/product-list' exact element={<div><NavBar /><Product /></div>}>
+        {/* <Route path='/' exact element={<Login />}></Route> */}
+        <Route path='/' exact element={<div><NavBar /><Product /></div>}>
         </Route>
         <Route path='/about' exact element={<div><NavBar /><About /></div>}></Route>
       </Routes>
